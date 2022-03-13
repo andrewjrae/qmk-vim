afd536d1a334d995b0e4f6fc8f723bf40c857765
Strange Moth <63575293+strange-moth@users.noreply.github.com>
2022-03-12 20:16:28 -0600

    update documentation and fix typo

--- src/mac_mode.h
+++ src/mac_mode.h
@@ -7,10 +7,11 @@
 extern bool vim_for_mac;
 
 #define VIM_MAC_NOMAC(a, b) (vim_for_mac ? (a) : (b))
-// The keycodes produced by this VIM_MAC_NOMAC are not constants, so they cannot be used in a case statement
-// In order to get around this, we can duplicate the switch statement
+// The results of this implimentation of VIM_MAC_NOMAC are not constant
+// expressions, which means they cannot be used in a case statement
+// In order to get around this restriction we can duplicate the switch statement
 // and force our keycode to evaluate as a constant expression in each
-#define VIM_REDUCE_NOMAC_MAC_TO_CONST(body) { \
+#define VIM_REDUCE_MAC_NOMAC_TO_CONST(body) { \
     if(vim_for_mac) { const bool vim_for_mac = true; body } \
     else { const bool vim_for_mac = false; body } \
 }
